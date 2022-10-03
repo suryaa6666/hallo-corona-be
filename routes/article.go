f031475d03bd15537b2f2c8f5fb83a860bc2fad5
suryaa6666 <suryaelidanto@gmail.com>
2022-10-03 20:24:28 +0700

    FIX edit image without upload again

--- routes/article.go
+++ routes/article.go
@@ -15,7 +15,7 @@ func ArticleRoutes(r *mux.Router) {
 
 	r.HandleFunc("/articles", h.FindArticles).Methods("GET")
 	r.HandleFunc("/article/{id}", h.GetArticle).Methods("GET")
-	r.HandleFunc("/article", middleware.Auth(middleware.UploadFile(h.CreateArticle), "member")).Methods("POST")
-	r.HandleFunc("/article/{id}", middleware.Auth(middleware.UploadFile(h.UpdateArticle), "member")).Methods("PATCH")
+	r.HandleFunc("/article", middleware.Auth(middleware.UploadFile(h.CreateArticle, ""), "member")).Methods("POST")
+	r.HandleFunc("/article/{id}", middleware.Auth(middleware.UploadFile(h.UpdateArticle, "edit"), "member")).Methods("PATCH")
 	r.HandleFunc("/article/{id}", middleware.Auth(h.DeleteArticle, "member")).Methods("DELETE")
 }
--- routes/users.go
+++ routes/users.go
@@ -16,6 +16,6 @@ func UserRoutes(r *mux.Router) {
 	r.HandleFunc("/users", middleware.Auth(h.FindUsers, "admin")).Methods("GET")
 	r.HandleFunc("/user/{id}", middleware.Auth(h.GetUser, "admin")).Methods("GET")
 	r.HandleFunc("/user", middleware.Auth(h.CreateUser, "admin")).Methods("POST")
-	r.HandleFunc("/user/{id}", middleware.Auth(middleware.UploadFile(h.UpdateUser), "member")).Methods("PATCH")
+	r.HandleFunc("/user/{id}", middleware.Auth(middleware.UploadFile(h.UpdateUser, "edit"), "member")).Methods("PATCH")
 	r.HandleFunc("/user/{id}", middleware.Auth(h.DeleteUser, "admin")).Methods("DELETE")
 }
